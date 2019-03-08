bc73eb08c8efc733ec646f8c3892ca72d675fa00
maainul <mainul080@yahoo.com>
2019-03-08 09:45:55 +0600

    basic if else

---
+++ 6.php_string.php
@@ -0,0 +1,16 @@
+<?php
+// string length
+echo strlen("Hello anik");
+echo "<br/>";
+
+//count the number of world in a string.
+echo str_word_count("hello world");
+echo "<br/>";
+
+//reverse a string 
+echo strrev("hello world");
+echo "<br/>";
+
+//Search For a Specific Text Within a String
+echo strpos("hello anik","anik");
+?>
---
+++ 8.php_constants_are_global.php
@@ -0,0 +1,9 @@
+<?php
+	define("GRETTING","Welcome to W3school.com");
+	
+	function myTest(){
+		echo GRETTING;
+	}
+	myTest();
+
+?>
